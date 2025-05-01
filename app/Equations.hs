47b4239eb57676a12d56bc02ff9eb41ccd9a6b35
lizard111 <elizabeth.switney@gmail.com>
2025-05-01 16:09:35 -0600

    removed functions that were not working,, going to reattempt and restructure code

--- app/Equations.hs
+++ app/Equations.hs
@@ -8,24 +8,6 @@ Current resources for equations:
 ** Space Mission Analysis and Design book ** 
 -}
 
-type Position = V2 Double
-type Velocity = V2 Double
-type Acceleration = V2 Double
-
-data OrbitalInfo = MkOrbitalInfo
-  { pos :: Position  -- position (m)
-  , vel :: Velocity  -- velocity (m/s)
-  } deriving (Show)
-
--- Vector space operations for OrbitalInfo
-addOrbital :: OrbitalInfo -> OrbitalInfo -> OrbitalInfo
-addOrbital (MkOrbitalInfo p1 v1) (MkOrbitalInfo p2 v2) =
-  MkOrbitalInfo (p1 + p2) (v1 + v2)
-
-scaleOrbital :: Double -> OrbitalInfo -> OrbitalInfo
-scaleOrbital s (MkOrbitalInfo p v) =
-  MkOrbitalInfo (s *^ p) (s *^ v)
-
 -- Universal gravitational constant (N*m^2/kg^2)
 gravConst :: Double 
 gravConst = 6.6743e-11
@@ -46,33 +28,3 @@ orbitPeriod radiO v = (2 * pi * radiO) / v
 altitude :: Double -> Double -> Double -> Double 
 altitude massP acc radiP = (sqrt ((gravConst * massP) / acc )) - radiP
 
--- Calculate gravitational acceleration at a position
-gravAccel :: Double -> Position -> Acceleration
-gravAccel mass (V2 x y) =
-  let r = sqrt (x*x + y*y)
-      a = -gravConst * mass / (r**3)
-  in V2 (a * x) (a * y)
-
--- State derivative for RK4
-stateDerivative :: Double -> OrbitalInfo -> OrbitalInfo
-stateDerivative mass (MkOrbitalInfo p v) =
-  MkOrbitalInfo v (gravAccel mass p)
-
--- Single simulation step (Runge-Kutta 4th order)
-simulateStep :: Double -> Double -> OrbitalInfo -> OrbitalInfo
-simulateStep mass dt state =
-  let k1 = stateDerivative mass state
-      k2 = stateDerivative mass (addOrbital state (scaleOrbital (dt/2) k1))
-      k3 = stateDerivative mass (addOrbital state (scaleOrbital (dt/2) k2))
-      k4 = stateDerivative mass (addOrbital state (scaleOrbital dt k3))
-      derivative = scaleOrbital (1/6) $
-                   addOrbital k1 $
-                   addOrbital (scaleOrbital 2 k2) $
-                   addOrbital (scaleOrbital 2 k3) k4
-  in addOrbital state (scaleOrbital dt derivative)
-
--- Generate orbit trajectory
-generateOrbit :: Double -> OrbitalInfo -> Double -> Double -> [OrbitalInfo]
-generateOrbit mass initialState dt duration =
-  take (floor (duration / dt)) $
-  iterate (simulateStep mass dt) initialState
--- app/Main.hs
+++ app/Main.hs
@@ -42,61 +42,15 @@ moon :: Planet
 moon = MkPlanet 0 0.07346e24 1738.1
 
 
--- ... (keep your existing Planet data and planet definitions)
-
--- Convert meters to screen coordinates
-scaleToScreen :: Double -> V2 Double -> Point
-scaleToScreen scale (V2 x y) =
-  (realToFrac x / realToFrac scale, realToFrac y / realToFrac scale)
-
--- Render the planet and orbit
-renderOrbit :: Planet -> [OrbitalInfo] -> Picture
-renderOrbit planet states =
-  let scaleFactor = 1e6  -- Adjust this for proper scaling
-      planetRadiusScreen = realToFrac (planetRadius planet * 1000) / realToFrac scaleFactor
-      orbitPath = line $ map (scaleToScreen scaleFactor . pos) states
-      currentPos = scaleToScreen scaleFactor (pos (head states))
-  in pictures
-     [ color blue (circleSolid planetRadiusScreen)  -- Planet
-     , color white orbitPath                      -- Orbit path
-     , translate (fst currentPos) (snd currentPos) $
-       color red (circleSolid 5)                  -- Satellite
-     ]
-
--- Create initial orbital state
-initialState :: Planet -> Double -> Double -> OrbitalInfo
-initialState planet altitude initialSpeed =
-  let r = planetRadius planet * 1000 + altitude  -- Convert km to m
-      position = V2 r 0
-      velocity = V2 0 initialSpeed
-  in MkOrbitalInfo position velocity
-
--- Animation function
-animateOrbit :: Planet -> Double -> Double -> IO ()
-animateOrbit planet altitude speed = do
-  let initState = initialState planet altitude speed
-      dt = 100  -- Time step in seconds
-      duration = 3600 * 2  -- 2 hours in seconds
-      orbit = generateOrbit (planetMass planet) initState dt duration
-      display = InWindow "Orbit Simulation" (800, 800) (10, 10)
-  animate display black (const (renderOrbit planet orbit))
-
-
 main :: IO ()
 main = do
-  putStrLn "Select a planet:"
-  putStrLn "1. Mercury"
-  putStrLn "2. Venus"
-  -- ... (add other planets)
-  putStrLn "Enter planet number:"
-  planetChoice <- readLn
-  
-  putStrLn "Enter altitude (meters):"
-  altitude <- readLn
-  
-  putStrLn "Enter initial speed (m/s):"
-  speed <- readLn
-  
+
+  animateOrbit earth 2000 7.8 -- alt == km ; vel == km/s
+
+  {-
+
+  if planet clicked do some sort of switch case to reinnitiate sim
+
   let planet = case planetChoice of
                  1 -> mercury
                  2 -> venus
@@ -108,3 +62,4 @@ main = do
                  8 -> neptune
                  0 -> moon
   animateOrbit planet altitude speed
+  -}
