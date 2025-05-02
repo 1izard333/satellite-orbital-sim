bf4dc05370cffcccb80d03e89fec5a13872640e5
lizard111 <elizabeth.switney@gmail.com>
2025-05-01 20:39:50 -0600

    updated equations

--- app/Equations.hs
+++ app/Equations.hs
@@ -1,30 +1,44 @@
 module Equations where
 
-import Linear
-
 {-
 Current resources for equations:
 ** https://www.physicsclassroom.com/class/circles/lesson-4/mathematics-of-satellite-motion ** 
 ** Space Mission Analysis and Design book ** 
 -}
 
+type Position = (Float, Float)
+
 -- Universal gravitational constant (N*m^2/kg^2)
-gravConst :: Double 
+gravConst :: Float 
 gravConst = 6.6743e-11
 
--- Orbital Speed Equation (v = √(G*M/R))
-velocity :: Double -> Double -> Double 
+-- Orbital Speed Equation (v = (GM/R)^(1/2))
+velocity :: Float -> Float -> Float 
 velocity massP radiO = sqrt((gravConst * massP) / radiO)
 
--- Centripetal acceleration (a = G*M/R²)
-acceleration :: Double -> Double -> Double 
+-- Centripetal acceleration (a = GM/R^2)
+acceleration :: Float -> Float -> Float 
 acceleration massP radiO = (gravConst * massP) / (radiO ** 2.0)
 
--- Orbital Period (T = 2πR/v)
-orbitPeriod :: Double -> Double -> Double 
+-- Orbital Period (T = 2piR/v)
+orbitPeriod :: Float -> Float -> Float 
 orbitPeriod radiO v = (2 * pi * radiO) / v
 
--- Altitude (alt = √(G*M/a) - R_planet)
-altitude :: Double -> Double -> Double -> Double 
+-- Altitude (alt = (GM/a)^(1/2) - R_planet)
+altitude :: Float -> Float -> Float -> Float 
 altitude massP acc radiP = (sqrt ((gravConst * massP) / acc )) - radiP
 
+-- Semi-minor axis = (a^2 )
+semiMinorAxis :: Float -> Float -> Float
+semiMinorAxis radiO ecc = sqrt ((radiO ** 2) - ((ecc ** 2)*(radiO ** 2)))
+
+-- convert km to m
+kmToMeters :: Float -> Float
+kmToMeters a = a * 1000  
+      
+-- orbital path/trajectory at time t
+orbitPosition :: Float -> Float -> Float -> Float -> Position
+orbitPosition a b omega t = (x, y)
+  where
+    x = a * cos (omega * t)
+    y = b * sin (omega * t)
--- dist-newstyle/tmp/src-57004/OneTuple-0.4.2/test/th.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module Main where
-
-import Data.Tuple.Solo
-import Data.Tuple.Solo.TH (tupE)
-
-main :: IO ()
-main = print $ MkSolo 'x' == $(tupE [[| 'x' |]])
