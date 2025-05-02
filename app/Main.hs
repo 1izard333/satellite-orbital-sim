adfd74a51a9d91db2f86d897ae6b38220f6a68b0
lizard111 <elizabeth.switney@gmail.com>
2025-05-02 08:26:16 -0600

    added image file path the planet object

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Graphics.Gloss
+--import Graphics.Gloss.Juicy
+--import Codec.Picture
 import Equations
 import Orbit
 
@@ -8,37 +10,37 @@ import Orbit
 -- planets list ( https://nssdc.gsfc.nasa.gov/planetary/factsheet/planet_table_ratio.html )
 
 mercury :: Planet
-mercury = MkPlanet 1 0.33010e24 2440.5
+mercury = MkPlanet 1 0.33010e24 2440.5 "resources/mercury.png"
 
 venus :: Planet
-venus = MkPlanet 2 4.8673e24 6051.8
+venus = MkPlanet 2 4.8673e24 6051.8 "resources/venus.png"
 
 earth :: Planet
-earth = MkPlanet 3 5.9722e24 6378.137
+earth = MkPlanet 3 5.9722e24 6378.137 "resources/earth.png"
 
 mars :: Planet
-mars = MkPlanet 4 0.64169e24 3396.2
+mars = MkPlanet 4 0.64169e24 3396.2 "resources/mars.png"
 
 jupiter :: Planet
-jupiter = MkPlanet 5 1898.13e24 71492
+jupiter = MkPlanet 5 1898.13e24 71492 "resources/jupiter.png"
 
 saturn :: Planet
-saturn = MkPlanet 6 568.32e24 60268
+saturn = MkPlanet 6 568.32e24 60268 "resources/saturn.png"
 
 uranus :: Planet
-uranus = MkPlanet 7 86.811e24 25559
+uranus = MkPlanet 7 86.811e24 25559 "resources/uranus.png"
 
 neptune :: Planet
-neptune = MkPlanet 8 102.409e24 24764
+neptune = MkPlanet 8 102.409e24 24764 "resources/neptune.png"
 
 moon :: Planet
-moon = MkPlanet 0 0.07346e24 1738.1
+moon = MkPlanet 0 0.07346e24 1738.1 "resources/moon.png"
 
 
 main :: IO ()
 main = do
 
-  animateOrbit neptune 2000 7.8  0 -- alt == km ; vel == km/s
+  animateOrbit earth 2000 200  0.7 -- alt == km ; vel == km/s
 
   {-
 
